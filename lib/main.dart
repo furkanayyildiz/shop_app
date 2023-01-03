cdd8fbac2b7a0f2c8da6c3fe238fd84f9d946476
Furkan Ayyıldız <furkan199905@gmail.com>
2023-01-03 14:25:51 +0300

    orders provider and cart clear method added

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import './screens/product_detail_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
 import './screens/cart_screen.dart';
+import './providers/orders.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (ctx) => Products()),
         ChangeNotifierProvider(create: (ctx) => Cart()),
+        ChangeNotifierProvider(create: (ctx) => Orders()),
       ],
       child: MaterialApp(
           title: 'MyShop',
